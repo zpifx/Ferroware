80154c5f9ce3149bdda33703ff48be8525d25700
zpifx <zpifx@proton.me>
2026-09-09 22:34:25 -0500

    C Basic Util B

---
+++ baseutils/5cc/src/main.c
@@ -0,0 +1,9 @@
+// baseutils/5cc/src/main.c
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+
+    return 0;
+}
---
+++ boot5/README.md
@@ -0,0 +1,2 @@
+# Overview
+A DOS-like bootloader, used to either start a 32/64-bit OS, and run 16-bit programs.
